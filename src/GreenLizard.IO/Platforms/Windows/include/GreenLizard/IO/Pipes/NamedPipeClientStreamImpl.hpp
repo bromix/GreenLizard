a244612f560a5f46d1987cb7abb9ac77fa113493
bromix <matthias.bromisch@gmail.com>
2022-08-05 20:11:43 +0200

    fix: added missing include guard

--- src/GreenLizard.IO/Platforms/Windows/include/GreenLizard/IO/Pipes/NamedPipeClientStreamImpl.hpp
+++ src/GreenLizard.IO/Platforms/Windows/include/GreenLizard/IO/Pipes/NamedPipeClientStreamImpl.hpp
@@ -1,31 +1,27 @@
+#pragma once
 #include "GreenLizard/Platform/Windows.hpp"
 #include "GreenLizard/Platform/Handle.hpp"
 #include "GreenLizard/String.hpp"
 
-namespace GreenLizard::IO::Pipes
-{
-    class NamedPipeClientStream::Impl final
-    {
+namespace GreenLizard::IO::Pipes {
+    class NamedPipeClientStream::Impl final {
     public:
-        explicit Impl(const String &pipeName)
-        {
+        explicit Impl(const String &pipeName) {
             this->pipeName = R"(\\.\pipe\)" + pipeName;
         }
 
-        void Connect(const uint32_t timeOut)
-        {
-            pipeHandle = CreateFile(pipeName.c_str(), GENERIC_READ | GENERIC_WRITE, 0, nullptr, OPEN_EXISTING, 0, nullptr);
-            if(pipeHandle.IsInvalid())
-            {
+        void Connect(const uint32_t timeOut) {
+            pipeHandle = ::CreateFile(pipeName.c_str(), GENERIC_READ | GENERIC_WRITE, 0, nullptr, OPEN_EXISTING, 0,
+                                    nullptr);
+            if (pipeHandle.IsInvalid()) {
                 throw std::runtime_error("Failed to connect to pipe");
             }
-            WaitNamedPipe(pipeName.c_str(), timeOut);
+            ::WaitNamedPipe(pipeName.c_str(), timeOut);
         }
 
-        void Write(const void *buffer, const uint32_t offset, const uint32_t length)
-        {
+        void Write(const void *buffer, const uint32_t offset, const uint32_t length) {
             DWORD bytesWritten = 0;
-            WriteFile(pipeHandle, buffer, length, &bytesWritten, nullptr);
+            ::WriteFile(pipeHandle, buffer, length, &bytesWritten, nullptr);
         }
 
     private:
