068364e0386b75aea4984f97b2e566943a3e9ded
bromix <matthias.bromisch@gmail.com>
2022-08-11 20:00:04 +0200

    cleanup

--- src/GreenLizard/include/GreenLizard/Ref.hpp
+++ src/GreenLizard/include/GreenLizard/Ref.hpp
@@ -5,9 +5,19 @@
 namespace GreenLizard
 {
 	// token from https://github.com/TheCherno/Hazel/blob/49da3c1e08245feb5b978269119f50b0873dc9d1/Hazel/src/Hazel/Core/Base.h#L38
+	/**
+	 * @brief A reference counted smart pointer.
+	 * @tparam T The type of the object to reference.
+	 */
 	template<typename T>
 	using Ref = std::shared_ptr<T>;
 
+	/**
+	 * @brief Creates a reference counted smart pointer.
+	 * @tparam T The type of the object to reference.
+	 * @param args The arguments to pass to the constructor of the object.
+	 * @return A reference counted smart pointer to the new object.
+	 */
 	template<typename T, typename ... Args>
 	constexpr Ref<T> CreateRef(Args&& ... args)
 	{
